b479138f9b20482d679418016c7ee98939b5a071
Ademola Hammed <demmarss@gmail.com>
2025-08-09 12:45:22 -0500

    Fix Decorator Syntax Error in Employee Model

--- backend/models/Employee.ts
+++ backend/models/Employee.ts
@@ -9,57 +9,57 @@ export class Employee extends Model {
   @PrimaryKey
   @AutoIncrement
   @Column(DataType.INTEGER)
-  declare id: number;
+  id!: number;
 
   @Column({
     type: DataType.STRING(255),
     allowNull: false,
   })
-  declare name: string;
+  name!: string;
 
   @Column({
     type: DataType.TEXT,
     allowNull: true,
   })
-  declare address?: string;
+  address?: string;
 
   @Column({
     type: DataType.JSON,
     allowNull: true,
   })
-  declare roles?: string[];
+  roles?: string[];
 
   @Column({
     type: DataType.DATE,
     allowNull: true,
   })
-  declare dateOfBirth?: Date;
+  dateOfBirth?: Date;
 
   @Column({
     type: DataType.STRING(20),
     allowNull: true,
   })
-  declare phoneNumber?: string;
+  phoneNumber?: string;
 
   @Column({
     type: DataType.STRING(255),
     allowNull: false,
     unique: true,
   })
-  declare email: string;
+  email!: string;
 
   @Column({
     type: DataType.STRING(255),
     allowNull: false,
   })
-  declare password: string;
+  password!: string;
 
   @Column({
     type: DataType.TEXT,
     allowNull: true,
   })
-  declare photoUrl?: string;
+  photoUrl?: string;
 
   @HasMany(() => EmployeePosition)
-  declare employeePositions: EmployeePosition[];
+  employeePositions!: EmployeePosition[];
 }
